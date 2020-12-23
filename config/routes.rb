d1a53d8252e5a79fd74856ac08c7a5bc5ed81c24
kurumin27 <oneokrock_20@yahoo.co.jp>
2020-12-23 22:47:46 +0900

    add cotroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'product/index'
+  get 'product/new'
+  get 'product/create'
+  get 'product/edit'
+  get 'product/update'
+  get 'product/destroy'
+  get 'product/show'
+  get 'reservation/index'
+  get 'reservation/new'
+  get 'reservation/create'
+  get 'reservation/edit'
+  get 'reservation/update'
+  get 'reservation/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
