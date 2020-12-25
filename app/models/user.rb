d0cf5c6fa7dff4cbc66f0fa5b7bf2e6c1f542930
kurumin27 <oneokrock_20@yahoo.co.jp>
2020-12-25 09:34:10 +0900

    devise setting and validation fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, uniqueness: true
   validates :telephone, presence: true, format: {with: /\A\d{10,11}\z/}, numericality: { only_integer: true }
-  validates :email, presence: true
-  validates :password, length: {minimum: 6}, presence: true
   validates :password, presence: true, confirmation: true
 
   has_many :orders
