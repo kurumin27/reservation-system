5b642a14ad7d8758ec084d0dbe8639deeb762b79
kurumin27 <oneokrock_20@yahoo.co.jp>
2020-12-27 21:05:06 +0900

    model file add

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  validates :product_name, presence: true
+  validates :price, presence: true, numericality: { only_integer: true }
+
+  belongs_to :reservation
 end
