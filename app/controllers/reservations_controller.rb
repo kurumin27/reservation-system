ee1bb4e34a9b0580aa2cd025cc8f70885722a293
kurumin27 <oneokrock_20@yahoo.co.jp>
2020-12-29 00:43:44 +0900

    reservation function add

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,11 +1,20 @@
 class ReservationsController < ApplicationController
+  before_action :product_name, only:[:new, :create, :edit, :update]
+  before_action :reservation_product, only:[:new, :create]
+
   def index
   end
 
   def new
+    @reservation = Reservation.new
   end
 
   def create
+    @reservation = Reservation.create(reservation_params)
+    
+    unless @reservation.save
+      render :new
+    end
   end
 
   def edit
@@ -16,4 +25,19 @@ class ReservationsController < ApplicationController
 
   def destroy
   end
+
+
+  private
+  def reservation_params
+    params.require(:reservation).permit(:date, :time, products_attributes: [:product_name, :_destroy, :id])
+  end
+
+  def product_name
+    @products = Product.select(:product_name)
+  end
+
+  def reservation_product
+    @product = Product.where(product_name: nil)
+  end
+
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,7 @@
 class Reservation < ApplicationRecord
   validates :date, presence: true
   validates :time, presence: true
-  validates :product_id, presence: true
+  validates :product_name, presence: true
 
   has_one :product
   belongs_to :user
