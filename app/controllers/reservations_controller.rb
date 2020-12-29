d7a030076b8e7f355ef8946293285485cfd460d6
kurumin27 <oneokrock_20@yahoo.co.jp>
2020-12-29 22:21:25 +0900

    modified reservation function

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,8 +1,9 @@
 class ReservationsController < ApplicationController
   before_action :product_name, only:[:new, :create, :edit, :update]
-  before_action :reservation_product, only:[:new, :create]
+  before_action :move_to_sign_in, only: [:index, :new, :edit] 
 
   def index
+      @reservation = Reservation.count(current_user.id)
   end
 
   def new
@@ -10,8 +11,7 @@ class ReservationsController < ApplicationController
   end
 
   def create
-    @reservation = Reservation.create(reservation_params)
-    
+    @reservation = Reservation.new(reservation_params)
     unless @reservation.save
       render :new
     end
@@ -29,15 +29,20 @@ class ReservationsController < ApplicationController
 
   private
   def reservation_params
-    params.require(:reservation).permit(:date, :time, products_attributes: [:product_name, :_destroy, :id])
+    params.require(:reservation).permit(:datetime, :product_id).merge(user_id: current_user.id)
   end
 
   def product_name
-    @products = Product.select(:product_name)
+    @products = Product.all
   end
 
-  def reservation_product
-    @product = Product.where(product_name: nil)
+  def move_to_sign_in
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
+  # def reservation_product
+  #   @product = Product.where(product_name: nil)
+  # end
 
 end
