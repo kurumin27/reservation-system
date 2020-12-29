080c510d8ddf28bd8e9bca0198027b66ced950b7
kurumin27 <oneokrock_20@yahoo.co.jp>
2020-12-29 17:09:12 +0900

    list page layout modified

--- app/admin/reservations.rb
+++ app/admin/reservations.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Reservation do
-  permit_params :date, :time, :user_id, :product_id
+  actions :index
 
   # 一覧ページの検索条件削除
   remove_filter :reservation
@@ -13,6 +13,18 @@ ActiveAdmin.register Reservation do
   filter :created_at
   filter :updated_at
 
+    # 一覧ページ
+    index do
+      column :id
+      column :date
+      column :time
+      column :user_id
+      column :product_name
+      column :created_at
+  
+      actions 
+    end
+  permit_params :date, :time, :user_id, :product_name
   # or
   #
   # permit_params do
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register User do
-
-  permit_params :name, :telephone, :email, :encrypted_password
+  actions :index
+  
   # 一覧ページの検索条件削除
   remove_filter :reservation
 
@@ -11,6 +11,17 @@ ActiveAdmin.register User do
   filter :created_at
   filter :updated_at
 
+  # 一覧ページ
+  index do
+    column :id
+    column :name
+    column :email
+    column :telephone
+    column :created_at
+
+    actions 
+  end
+  permit_params :name, :telephone, :email
   # or
   # permit_params do
   #   permitted = [:name, :telephone, :email, :encrypted_password, :remember_created_at]
