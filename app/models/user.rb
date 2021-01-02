8a841616c6098c87a92976d76c8a4690a884add9
kurumin27 <oneokrock_20@yahoo.co.jp>
2021-01-02 19:04:00 +0900

    add guest login function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.password_confirmation = user.password
+      user.name = "ゲスト"
+      user.telephone = "08012345678"
+    end
+  end
 
   validates :name, presence: true, uniqueness: true
   validates :telephone, presence: true, format: {with: /\A\d{10,11}\z/}, numericality: { only_integer: true }
